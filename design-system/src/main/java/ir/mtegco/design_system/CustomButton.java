af6be7157c2302cc257a4bffeea297acedddb293
Matin <matink1490@gmail.com>
2022-03-01 15:30:56 +0330

    init commit

---
+++ design-system/src/main/java/ir/mtegco/design_system/CustomButton.java
@@ -0,0 +1,23 @@
+package ir.mtegco.design_system;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+public class CustomButton extends RelativeLayout {
+    public CustomButton(Context context) {
+        super(context);
+    }
+
+    public CustomButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public CustomButton(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
