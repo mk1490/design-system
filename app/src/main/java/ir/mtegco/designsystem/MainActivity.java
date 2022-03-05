417c7c930189a58e4687492ac946d321007dc4a1
Matin <matink1490@gmail.com>
2022-03-05 10:36:53 +0330

    Change toolbar

--- app/src/main/java/ir/mtegco/designsystem/MainActivity.java
+++ app/src/main/java/ir/mtegco/designsystem/MainActivity.java
@@ -1,15 +1,27 @@
 package ir.mtegco.designsystem;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import ir.mtegco.design_system.Toolbar.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) getWindow().getDecorView().findViewWithTag("toolbar");
+        toolbar.setTitle("Matin");
+        toolbar.setOnBackClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
--- design-system/src/main/java/ir/mtegco/design_system/Toolbar/Toolbar.java
+++ design-system/src/main/java/ir/mtegco/design_system/Toolbar/Toolbar.java
@@ -3,19 +3,29 @@ package ir.mtegco.design_system.Toolbar;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.util.AttributeSet;
+import android.util.TypedValue;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.core.content.ContextCompat;
 
 import ir.mtegco.design_system.R;
 
 public class Toolbar extends RelativeLayout {
-    TextView title, description;
+    TextView title, subtitle;
     RelativeLayout back;
-    OnClickListener onBackClickListener;
+    LinearLayout linearLayout;
+    OnClickListener onBackClickListener = null;
+    int actionBarSize;
 
     public void setOnBackClickListener(OnClickListener onBackClickListener) {
         this.onBackClickListener = onBackClickListener;
+        back.setVisibility(VISIBLE);
+        linearLayout.setPaddingRelative(0, 0, actionBarSize, 0);
     }
 
     public Toolbar(Context context) {
@@ -42,24 +52,43 @@ public class Toolbar extends RelativeLayout {
         View view = inflate(getContext(), R.layout.designsystem_toolbar, null);
         setupViews(view);
         TypedArray typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.Toolbar);
+        TypedValue typedValue = new TypedValue();
+        getContext().getTheme().resolveAttribute(android.R.attr.actionBarSize, typedValue, true);
+        actionBarSize = TypedValue.complexToDimensionPixelSize(typedValue.data, getResources().getDisplayMetrics());
+        LayoutParams layoutParams = new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, actionBarSize);
+
         if (typedArray.hasValue(R.styleable.Toolbar_title)) {
             title.setText(typedArray.getString(R.styleable.Toolbar_title));
         }
-        if (typedArray.hasValue(R.styleable.Toolbar_description)) {
-            description.setText(typedArray.getString(R.styleable.Toolbar_description));
-            description.setVisibility(VISIBLE);
+        if (typedArray.hasValue(R.styleable.Toolbar_subtitle)) {
+            subtitle.setText(typedArray.getString(R.styleable.Toolbar_subtitle));
+            subtitle.setVisibility(VISIBLE);
         }
-        if (onBackClickListener != null) {
-            back.setVisibility(VISIBLE);
-        } else {
+        if (onBackClickListener == null) {
             back.setVisibility(GONE);
         }
-        back.setOnClickListener(onBackClickListener);
+        back.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (onBackClickListener != null) {
+                    onBackClickListener.onClick(view);
+                }
+            }
+        });
+        view.setLayoutParams(layoutParams);
+        addView(view);
     }
 
     private void setupViews(View view) {
         title = (TextView) view.findViewById(R.id.title);
-        description = (TextView) view.findViewById(R.id.description);
+        subtitle = (TextView) view.findViewById(R.id.subtitle);
         back = (RelativeLayout) view.findViewById(R.id.back);
+        linearLayout = (LinearLayout) view.findViewById(R.id.linearLayout);
     }
+
+
+    public void setTitle(String title) {
+        this.title.setText(title);
+    }
+
 }
